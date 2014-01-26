34f0faa1beba11a4b64132e45f99e01b7de1e554
sasa1977 <sasa.juric@gmail.com>
2014-01-26 20:48:05 +0100

    separate module for default interface funs

--- lib/exactor.ex
+++ lib/exactor.ex
@@ -9,61 +9,10 @@ defmodule ExActor do
     quote do
       use GenServer.Behaviour
       
-      import ExActor.Macros, only: [
-        definit: 1, definit: 2,
-        defcall: 2, defcall: 3,
-        defcast: 2, defcast: 3,
-        definfo: 2, definfo: 3,
-
-        handle_call_response: 2, handle_cast_response: 2,
-        initial_state: 1, new_state: 1, reply: 2, set_and_reply: 2,
-        delegate_to: 2
-      ]
-      unquote(interface_funs(__CALLER__))
+      import ExActor.Macros
+      use ExActor.DefaultInterface
       
       @exported HashSet.new
-    end
-  end
-  
-  defp interface_funs(caller) do
-    quote do
-      def start(args // nil, options // []) do
-        :gen_server.start(unquote_splicing(start_args(caller)))
-      end
-      
-      def start_link(args // nil, options // []) do
-        :gen_server.start_link(unquote_splicing(start_args(caller)))
-      end
-
-      unquote(def_initializer(caller))
-    end
-  end
-
-  defp start_args(caller) do
-    defargs = [quote(do: __MODULE__), quote(do: args), quote(do: options)]
-    case Module.get_attribute(caller.module, :exactor_global_options)[:export] do
-      default when default in [nil, false, true] -> defargs
-      
-      local_name when is_atom(local_name) ->
-        [quote(do: {:local, unquote(local_name)}) | defargs]
-      
-      {:local, local_name} ->
-        [quote(do: {:local, unquote(local_name)}) | defargs]
-      
-      {:global, global_name} ->
-        [quote(do: {:global, unquote(global_name)}) | defargs]
-      
-      _ -> defargs
-    end
-  end
-
-  defp def_initializer(caller) do
-    case Module.get_attribute(caller.module, :exactor_global_options)[:initial_state] do
-      nil -> nil
-      state ->
-        quote do
-          def init(_), do: initial_state(unquote(state))
-        end
     end
   end
 end
---
+++ lib/exactor/default_interface.ex
@@ -0,0 +1,49 @@
+defmodule ExActor.DefaultInterface do
+  defmacro __using__(_) do
+    quote do
+      unquote(interface_funs(__CALLER__))
+    end
+  end
+
+  defp interface_funs(caller) do
+    quote do
+      def start(args // nil, options // []) do
+        :gen_server.start(unquote_splicing(start_args(caller)))
+      end
+      
+      def start_link(args // nil, options // []) do
+        :gen_server.start_link(unquote_splicing(start_args(caller)))
+      end
+
+      unquote(def_initializer(caller))
+    end
+  end
+
+  defp start_args(caller) do
+    defargs = [quote(do: __MODULE__), quote(do: args), quote(do: options)]
+    case Module.get_attribute(caller.module, :exactor_global_options)[:export] do
+      default when default in [nil, false, true] -> defargs
+      
+      local_name when is_atom(local_name) ->
+        [quote(do: {:local, unquote(local_name)}) | defargs]
+      
+      {:local, local_name} ->
+        [quote(do: {:local, unquote(local_name)}) | defargs]
+      
+      {:global, global_name} ->
+        [quote(do: {:global, unquote(global_name)}) | defargs]
+      
+      _ -> defargs
+    end
+  end
+
+  defp def_initializer(caller) do
+    case Module.get_attribute(caller.module, :exactor_global_options)[:initial_state] do
+      nil -> nil
+      state ->
+        quote do
+          def init(_), do: initial_state(unquote(state))
+        end
+    end
+  end
+end
