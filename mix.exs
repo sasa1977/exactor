053828ba100efa05e482d80de731322aeecb6813
Sasa Juric <sasa.juric@gmail.com>
2014-05-19 17:47:05 +0200

    use Elixir 0.13.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExActor.Mixfile do
     [
       project: "ExActor",
       version: "0.4.0-dev",
-      elixir: "~> 0.13.1",
+      elixir: "~> 0.13.2",
       app: :exactor,
       deps: deps,
       package: [
