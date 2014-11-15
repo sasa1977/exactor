26504c35cbabb7d9386393aa3427885ad7d532fb
Sasa Juric <sasa.juric@gmail.com>
2014-11-15 21:00:22 +0100

    remove expm package

--- package.exs
+++
@@ -1,6 +0,0 @@
-Expm.Package.new(
-  name: "exactor", description: "Helpers for easier implementation of actors in Elixir",
-  version: "0.1", keywords: ["elixir", "actor"], 
-  maintainers: [[name: "Sasa Juric", email: "sasa.juric@gmail.com"]],
-  repositories: [[github: "sasa1977/exactor"]]
-)
